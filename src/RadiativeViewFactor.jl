0b7ed3bf573935dd01d2e540058ca3672eee0d7b
Alex Coxe <rot4te@gmail.com>
2026-07-03 19:45:08 -0400

    added .re2 mesh compatibility and tests

--- src/RadiativeViewFactor.jl
+++ src/RadiativeViewFactor.jl
@@ -22,7 +22,7 @@ include("Assembly.jl")      # imports Results; defines register_gpu_hook!
 include("GPUAssembly.jl")   # imports Results + Assembly.register_gpu_hook!;
                              # calls register_gpu_hook!(compute_view_factors_gpu)
 
-using .MeshIO:    load_mesh, MeshData
+using .MeshIO:    load_mesh, load_re2, MeshData
 using .MeshIO:    SurfaceElement
 using .Geometry:  quad8_physical_point, quad8_normal_and_area_element,
                   quad4_shape, quad4_physical_point, quad4_normal_and_area_element,
@@ -33,6 +33,7 @@ using .Results:   ViewFactorResult, aggregate_by_group,
 using .Assembly:  compute_view_factors
 
 export load_mesh,
+       load_re2,
        compute_view_factors,
        aggregate_by_group,
        check_reciprocity,
